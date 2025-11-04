15b1b3f5a8cd3be1463f3b0c385013be0c590bd5
hyewon3938 <hyewon3938@naver.com>
2025-11-04 17:19:53 +0900

    feat : favicon 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import StyledComponentsRegistry from "@/lib/StyledComponentsRegistry";
 export const metadata: Metadata = {
   title: "리커밋 | 즐거운 독서의 시작",
   description: "리커밋 브랜드 소개 페이지",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
