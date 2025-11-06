24d693c6881a0989f71b3241bfc557de808d8a84
hyewon3938 <hyewon3938@naver.com>
2025-11-06 16:26:46 +0900

    feat: Collection 섹션 기본 데이터 및 임시 디자인 추가

--- src/sections/About/AboutSection.tsx
+++ src/sections/About/AboutSection.tsx
@@ -20,7 +20,9 @@ const AboutSection = () => {
           <p>읽는다는 건 결국 이어가는 일이라고 믿습니다.</p>
           <p>
             매일 펼치지 않아도, 다시 펼치게 되는 그 마음에 리커밋이 기분 좋은
-            이유가 되고 싶습니다. <br />
+            이유가 되고 싶습니다.
+          </p>
+          <p>
             이제는 끈갈피에 이어 비즈 식물 작품을 함께 선보이며 독서의 즐거움이
             머무는 공간에도 작은 평화를 더하고 있습니다.
           </p>
@@ -50,10 +52,11 @@ const Title = styled.h1`
 
 const ContentsWrap = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-end;
   gap: 20px;
   @media (max-width: ${({ theme }) => theme?.breakpoints?.mobile}) {
     flex-wrap: wrap;
+    align-items: center;
   }
 `;
 
@@ -71,4 +74,5 @@ const ImageWrap = styled.div`
 
 const TextWrap = styled.div`
   word-break: keep-all;
+  line-height: 1.2;
 `;
--- src/sections/Collection/CollectionSection.tsx
+++ src/sections/Collection/CollectionSection.tsx
@@ -1,16 +1,82 @@
 import styled from "styled-components";
+import Image from "next/image";
+import apple01 from "@/assets/images/collection/apple01.jpg";
+import apple02 from "@/assets/images/collection/apple02.jpg";
+import greenTree01 from "@/assets/images/collection/greenTree01.jpg";
+import greenTree02 from "@/assets/images/collection/greenTree02.jpg";
+import stb01 from "@/assets/images/collection/stb01.jpeg";
+import stb02 from "@/assets/images/collection/stb02.jpeg";
+import bird01 from "@/assets/images/collection/bird01.jpeg";
+import bird02 from "@/assets/images/collection/bird02.jpeg";
+import plant01 from "@/assets/images/collection/plant01.jpeg";
+import plant02 from "@/assets/images/collection/plant02.jpeg";
+import plant03 from "@/assets/images/collection/plant03.jpeg";
 
 const CollectionSection = () => {
-  return <Wrap id="collection">collection</Wrap>;
+  return (
+    <Wrap id="collection">
+      <Title>Collection</Title>
+
+      <ItemWrap>
+        <Image src={greenTree01} alt="prd1" />
+        <Image src={greenTree02} alt="prd2" />
+      </ItemWrap>
+      <ItemWrap>
+        <Image src={stb01} alt="prd1" />
+        <Image src={stb02} alt="prd2" />
+      </ItemWrap>
+      <ItemWrap>
+        <Image src={apple01} alt="prd1" />
+        <Image src={apple02} alt="prd2" />
+      </ItemWrap>
+      <ItemWrap>
+        <Image src={bird02} alt="prd2" />
+        <Image src={bird01} alt="prd1" />
+      </ItemWrap>
+      <SingleImageWrap>
+        <Image src={plant01} alt="prd1" />
+        <Image src={plant02} alt="prd1" />
+        <Image src={plant03} alt="prd1" />
+      </SingleImageWrap>
+    </Wrap>
+  );
 };
 
 export default CollectionSection;
 
 const Wrap = styled.section`
+  width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100vh;
   background-color: #fff;
+  padding: 80px 20px 40px;
+  gap: 20px;
+  @media (max-width: ${({ theme }) => theme?.breakpoints?.mobile}) {
+    padding: 20px;
+    gap: 10px;
+  }
+`;
+const Title = styled.h1`
+  font-family: "DosGothic";
+`;
+
+const ItemWrap = styled.div`
+  display: flex;
+  width: 100%;
+  > img {
+    width: 50%;
+    height: auto;
+  }
+`;
+
+const SingleImageWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  > img {
+    width: inherit;
+    height: auto;
+  }
 `;
