e421b9a5606110e095148480a86a1c8d1cfb461b
hyewon3938 <hyewon3938@naver.com>
2025-11-06 16:34:05 +0900

    fix : 인앱에서 Footer 스크롤 시 안 보이는 이슈 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,8 @@ export default function HomePage() {
         <CollectionSection />
         <SpaceSection />
         <ContactSection />
+        <Footer />
       </ContentsWrap>
-      <Footer />
     </>
   );
 }
